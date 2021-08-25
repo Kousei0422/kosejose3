94a7de07b31782cf012f32f30089dd8530d60980
Kosei Takahashi <kousei0422.hero@gmail.com>
2021-08-25 14:30:57 -0700

    except footer

--- ps.js
+++ ps.js
@@ -1,11 +1,31 @@
-var button = document.getElementById('button');
+// 
 
-button.onclick = function() {
-    var div = document.getElementById('psconsole');
-    if (div.style.display !== 'none') {
-        div.style.display = 'none';
-    }
-    else {
-        div.style.display = 'block';
-    }
+let $button1 = document.getElementById("button1");
+let $button2 = document.getElementById("button2");
+let $psconsole = document.getElementById("psconsole");
+let $psdegital = document.getElementById("psdegital");
+
+reset_styles = function(){
+    $button1.classList.remove("active");
+    $button2.classList.remove("active");
+    $psconsole.classList.remove("active");
+    $psdegital.classList.remove("active");
 };
+
+$button1.addEventListener("click",function(){
+    reset_styles();
+    if (this.classList.toggle("active")){
+        $psconsole.classList.toggle("active");
+    }
+});
+$button2.addEventListener("click",function(){
+    reset_styles();
+    if (this.classList.toggle("active")){
+        $psdegital.classList.toggle("active");
+    }
+});
+
+$button1.classList.toggle("active");
+$psconsole.classList.toggle("active");
+
+
